df26b48a300c105eba8af6d3ae71415e1124f36a
Digambar <deshawaldigambar@gmail.com>
2022-09-19 17:24:13 +0530

    feat : added routes inside classes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
 import { Routes, Route} from "react-router-dom";
-import { Home, Pomodoro, Task, Login, Signup} from "./frontend/pages/index"
+import { Home, Pomodoro, Task, Login, Signup, Profile} from "./frontend/pages/index";
+import {Navbar, Footer, Aside} from "./frontend/components/index";
 
 function App() {
   return (
-
-    <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/login" element={ <Login />} />
-      <Route path="/signup" element={ <Signup />} />
-      <Route path="/task" element={ <Task />} />
-      <Route path="/pomodoro" element={ <Pomodoro />} />
-    </Routes>
-
-    )
+    <>
+      <div className="app">
+        <Navbar />
+        <div className="main-body-container">
+          <Aside />
+          <div className="main-page">
+              <Routes>
+                <Route path="/" element={ <Home />} />
+                <Route path="/login" element={ <Login />} />
+                <Route path="/signup" element={ <Signup />} />
+                <Route path="/task" element={ <Task />} />
+                <Route path="/pomodoro" element={ <Pomodoro />} />
+                <Route path="/user-profile" element={ <Profile/>} />
+              </Routes>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+    
+  )
 }
 
 export default App;
