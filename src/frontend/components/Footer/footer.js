160616d2668e1ffd55ed4fadb43a61f8053c2663
Digambar <deshawaldigambar@gmail.com>
2022-09-21 13:36:01 +0530

    feat : added social links

--- src/frontend/components/Footer/footer.js
+++ src/frontend/components/Footer/footer.js
@@ -1,13 +1,35 @@
 import "./footer.css";
+import github from "../../assets/github.png";
+import twitter from "../../assets/twitter.png";
+import linkedin from "../../assets/linkedin.png";
+
 export function Footer() {
 	return (
 		<>
 			<div className="video-lib-footer">
 				<p className="footer-heading">Digambar Deshawal</p>
 				<section className="social-handles">
-					<span className="material-icons-outlined md-36">person</span>
-					<span className="material-icons-outlined md-36">person</span>
-					<span className="material-icons-outlined md-36">person</span>
+					<a
+						href="https://twitter.com/deshawald14"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img src={twitter} alt="twitter" />
+					</a>
+					<a
+						href="https://www.linkedin.com/in/digambar-deshawal-9b279b147/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img src={linkedin} alt="linkedin" />
+					</a>
+					<a
+						href="https://github.com/Digambar1411"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img src={github} alt="github" />{" "}
+					</a>
 				</section>
 			</div>
 		</>
