bf8f4b1a2f65dd1c19877739e2ecffd20f39adcf
Digambar <deshawaldigambar@gmail.com>
2022-09-23 20:57:55 +0530

    minor changes

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -1,5 +1,6 @@
 import { Navbar } from "./Navbar/navbar";
 import { Footer } from "./Footer/footer";
 import { Aside} from "./Aside/aside"
+import { TaskModal} from "./TaskModal/taskModal"
 
-export {Navbar, Footer, Aside}
+export {Navbar, Footer, Aside,TaskModal}
