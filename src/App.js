3e24174959ec4016a31da0f9c2fc4069f9fb098e
Digambar <deshawaldigambar@gmail.com>
2022-10-10 13:27:54 +0530

    added private routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route} from "react-router-dom";
 import { Home, Pomodoro, Task, Login, Signup, Profile} from "./frontend/pages/index";
-import {Navbar, Footer, Aside} from "./frontend/components/index";
+import {Navbar, Footer, Aside, PrivateRoutes} from "./frontend/components/index";
 
 function App() {
   return (
@@ -15,10 +15,24 @@ function App() {
                 <Route path="/" element={ <Home />} />
                 <Route path="/login" element={ <Login />} />
                 <Route path="/signup" element={ <Signup />} />
-                <Route path="/task" element={ <Task />} />
-                <Route path="/pomodoro/:id" element={ <Pomodoro />} />
-                <Route path="/user-profile" element={ <Profile/>} />
-              </Routes>
+                <Route path="/task" element={ 
+                  < PrivateRoutes>
+                    <Task /> 
+                  </PrivateRoutes>} 
+                />
+                
+                <Route path="/pomodoro/:id" element={   
+                  < PrivateRoutes>
+                    <Pomodoro />
+                  </PrivateRoutes>} 
+                />
+
+                <Route path="/user-profile" element= {
+                  < PrivateRoutes>
+                    <Profile />
+                  </PrivateRoutes>} 
+                />
+                </Routes>
           </div>
         </div>
         <Footer />
--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -2,5 +2,7 @@ import { Navbar } from "./Navbar/navbar";
 import { Footer } from "./Footer/footer";
 import { Aside} from "./Aside/aside"
 import { TaskModal} from "./TaskModal/taskModal"
+import { Timer } from "./Timer/timer";
+import { PrivateRoutes } from "./Private routes/privateRoutes";
 
-export {Navbar, Footer, Aside,TaskModal}
+export {Navbar, Footer, Aside, TaskModal, Timer, PrivateRoutes}
